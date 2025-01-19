c0c9e887d2db45ab3d213e574467d37d6daa830e
Daniel-derf <danielpardinho321@gmail.com>
2025-01-19 17:05:37 -0300

    refactor: using nextjs link component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <h1>home page</h1>
       <p>
-        <a href="/about">About us</a>
+        <Link href="/about">About us</Link>
       </p>
     </div>
   );
