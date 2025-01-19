4720b0da9f8e77092238c20a6f490c82b7293a7a
Daniel-derf <danielpardinho321@gmail.com>
2025-01-19 17:01:42 -0300

    initial setup

---
+++ src/app/about/page.jsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "../page.module.css";
+
+export default function AboutPage() {
+  return (
+    <div className={styles.page}>
+      <h1>about page</h1>
+    </div>
+  );
+}
